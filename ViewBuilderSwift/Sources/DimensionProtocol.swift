3db343c2af10d198861162890c2becb6bf1621d7
Presto <yoohan95@gmail.com>
2019-08-08 14:28:52 +0900

    Write DimensionProtocol

---
+++ ViewBuilderSwift/Sources/DimensionProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  DimensionProtocol.swift
+//  ViewBuilderSwift
+//
+//  Created by Presto on 08/08/2019.
+//  Copyright © 2019 presto. All rights reserved.
+//
+
+import UIKit
+
+protocol DimensionProtocol { }
+
+extension Int: DimensionProtocol { }
+
+extension Double: DimensionProtocol { }
+
+extension CGFloat: DimensionProtocol { }
