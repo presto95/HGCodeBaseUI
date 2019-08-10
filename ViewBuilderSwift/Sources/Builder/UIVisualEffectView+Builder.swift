32d8dd3b381a391a10a9b5ccd3c17ad2db0caab9
Presto <yoohan95@gmail.com>
2019-08-10 20:48:00 +0900

    Write UIVisualEffectView builder

---
+++ ViewBuilderSwift/Sources/Builder/UIVisualEffectView+Builder.swift
@@ -0,0 +1,29 @@
+//
+//  UIVisualEffectView+Builder.swift
+//  ViewBuilderSwift
+//
+//  Created by Presto on 10/08/2019.
+//  Copyright © 2019 presto. All rights reserved.
+//
+
+#if os(iOS)
+
+extension Builder where Base: UIVisualEffectView {
+  
+  // MARK: - Retrieving View Information
+  
+  @discardableResult
+  func effect(_ effect: VisualEffect) -> Builder {
+    switch effect {
+    case let .blur(style):
+      let blurEffect = UIBlurEffect(style: style)
+      base.effect = blurEffect
+    case let .vibrancy(style):
+      let vibrancyEffect = UIVibrancyEffect(blurEffect: .init(style: style))
+      base.effect = vibrancyEffect
+    }
+    return self
+  }
+}
+
+#endif
---
+++ ViewBuilderSwift/Sources/Enum/VisualEffect.swift
@@ -0,0 +1,18 @@
+//
+//  VisualEffect.swift
+//  ViewBuilderSwift
+//
+//  Created by Presto on 10/08/2019.
+//  Copyright © 2019 presto. All rights reserved.
+//
+
+#if os(iOS)
+
+enum VisualEffect {
+  
+  case blur(UIBlurEffect.Style)
+  
+  case vibrancy(UIBlurEffect.Style)
+}
+
+#endif
