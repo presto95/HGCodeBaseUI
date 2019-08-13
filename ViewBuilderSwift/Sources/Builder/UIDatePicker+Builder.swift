06c6dda5ac2f454b478412a72097dca5313a660a
Presto <yoohan95@gmail.com>
2019-08-13 23:46:16 +0900

    End of UIDatePicker

--- ViewBuilderSwift/Sources/Builder/UIDatePicker+Builder.swift
+++ ViewBuilderSwift/Sources/Builder/UIDatePicker+Builder.swift
@@ -10,20 +10,65 @@
 
 public extension Builder where Base: UIDatePicker {
   
+  // MARK: - Helper
+  
+  /// Helper method associated with **date** setting feature.
+  ///
+  /// - `range` case for `minimumDate` and `maximumDate` property setter.
+  /// - `date` case for `date` property setter.
+  /// - `min` case for `minimumDate` property setter.
+  /// - `max` case for `maximumDate` property setter.
+  @discardableResult
+  func date(_ date: DatePickerBuilder.DateType) -> Builder {
+    switch date {
+    case let .each(date, min, max):
+      base.date = date
+      base.minimumDate = min
+      base.maximumDate = max
+    case let .range(min, max):
+      base.minimumDate = min
+      base.maximumDate = max
+    case let .date(date):
+      base.date = date
+    case let .min(min):
+      base.minimumDate = min
+    case let .max(max):
+      base.maximumDate = max
+    }
+    return self
+  }
+  
   // MARK: - Managing the Date and Calendar
   
+  /// Wrapper of `calendar` property setter.
   @discardableResult
   func calendar(_ calendar: Calendar!) -> Builder {
     base.calendar = calendar
     return self
   }
   
+  /// Wrapper of `date` property setter.
+  @discardableResult
+  func date(_ date: Date) -> Builder {
+    base.date = date
+    return self
+  }
+  
+  /// Wrapper of `setDate(_:animated:)` method.
+  @discardableResult
+  func date(_ date: Date, animated: Bool) -> Builder {
+    base.setDate(date, animated: animated)
+    return self
+  }
+  
+  /// Wrapper of `locale` property setter.
   @discardableResult
   func locale(_ locale: Locale?) -> Builder {
     base.locale = locale
     return self
   }
   
+  /// Wrapper of `timeZone` property setter.
   @discardableResult
   func timeZone(_ timeZone: TimeZone?) -> Builder {
     base.timeZone = timeZone
@@ -32,42 +77,37 @@ public extension Builder where Base: UIDatePicker {
   
   // MARK: - Configuring the Date Picker Mode
   
+  /// Wrapper of `datePickerMode` property setter.
   @discardableResult
   func datePickerMode(_ mode: UIDatePicker.Mode) -> Builder {
     base.datePickerMode = mode
     return self
   }
   
-  // MARK: - Managing the Date and Calendar & Configuring Temporal Attributes
+  // MARK: - Configuring Temporal Attributes
   
+  /// Wrapper of `maximumDate` property setter.
   @discardableResult
-  func date(_ date: DatePickerBuilder.DateType) -> Builder {
-    switch date {
-    case let .each(date, maximumDate, minimumDate):
-      base.date = date
-      base.maximumDate = maximumDate
-      base.minimumDate = minimumDate
-    case let .range(maximumDate, minimumDate):
-      base.maximumDate = maximumDate
-      base.minimumDate = minimumDate
-    case let .date(date):
-      base.date = date
-    case let .maximum(date):
-      base.maximumDate = date
-    case let .minimum(date):
-      base.minimumDate = date
-    }
+  func maximumDate(_ date: Date?) -> Builder {
+    base.maximumDate = date
     return self
   }
   
-  // MARK: - Configuring Temporal Attributes
+  /// Wrapper of `minimumDate` property setter.
+  @discardableResult
+  func minimumDate(_ date: Date?) -> Builder {
+    base.minimumDate = date
+    return self
+  }
   
+  /// Wrapper of `minuteInterval` property setter.
   @discardableResult
   func minuteInterval(_ interval: Int) -> Builder {
     base.minuteInterval = interval
     return self
   }
   
+  /// Wrapper of `countDownDuration` property setter.
   @discardableResult
   func countDownDuration(_ duration: TimeInterval) -> Builder {
     base.countDownDuration = duration
