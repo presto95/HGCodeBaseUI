99295b57c527c5341dff5829e0160d39a2380d93
Presto <yoohan95@gmail.com>
2019-08-13 23:46:00 +0900

    End of UICollectionViewCell

--- ViewBuilderSwift/Sources/Builder/UICollectionViewCell+Builder.swift
+++ ViewBuilderSwift/Sources/Builder/UICollectionViewCell+Builder.swift
@@ -10,30 +10,52 @@
 
 public extension Builder where Base: UICollectionViewCell {
   
-  // MARK: - Accessing the Cell's Views
+  // MARK: - Helper
   
+  /// Helper method associated with **background view** setting feature.
+  ///
+  /// - `plain` case for `backgroundView` property setter.
+  /// - `selected` case for `selectedBackgroundView` property setter.
   @discardableResult
   func backgroundView(_ view: CollectionViewCellBuilder.BackgroundView) -> Builder {
     switch view {
-    case let .each(normal, selected):
-      base.backgroundView = normal
+    case let .each(plain, selected):
+      base.backgroundView = plain
       base.selectedBackgroundView = selected
-    case let .normal(view):
-      base.backgroundView = view
-    case let .selected(view):
-      base.selectedBackgroundView = view
+    case let .plain(plain):
+      base.backgroundView = plain
+    case let .selected(selected):
+      base.selectedBackgroundView = selected
     }
+    return self
+  }
+  
+  // MARK: - Accessing the Cell's Views
+  
+  /// Wrapper of `backgroundView` property setter.
+  @discardableResult
+  func backgroundView(_ view: UIView?) -> Builder {
+    base.backgroundView = view
+    return self
+  }
+  
+  /// Wrapper of `selectedBackgroundView` property setter.
+  @discardableResult
+  func selectedBackgroundView(_ view: UIView?) -> Builder {
+    base.selectedBackgroundView = view
     return self
   }
   
   // MARK: - Managing the Cell's State
   
+  /// Wrapper of `isSelected` property setter.
   @discardableResult
   func isSelected(_ flag: Bool) -> Builder {
     base.isSelected = flag
     return self
   }
   
+  /// Wrapper of `isHighlighted` property setter.
   @discardableResult
   func isHighlighted(_ flag: Bool) -> Builder {
     base.isHighlighted = flag
