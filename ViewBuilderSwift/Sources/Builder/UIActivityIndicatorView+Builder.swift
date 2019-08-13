bce6035c7c2294f2f09a4ee295832433aec909c2
Presto <yoohan95@gmail.com>
2019-08-13 23:45:24 +0900

    End of UIActivityIndicatorView

--- ViewBuilderSwift/Sources/Builder/UIActivityIndicatorView+Builder.swift
+++ ViewBuilderSwift/Sources/Builder/UIActivityIndicatorView+Builder.swift
@@ -12,6 +12,7 @@ public extension Builder where Base: UIActivityIndicatorView {
   
   // MARK: - Managing an Activity Indicator
   
+  /// Wrapper of `hidesWhenStopped` property setter.
   @discardableResult
   func hidesWhenStopped(_ flag: Bool) -> Builder {
     base.hidesWhenStopped = flag
@@ -20,12 +21,14 @@ public extension Builder where Base: UIActivityIndicatorView {
   
   // MARK: - Configuring the Activity Indicator Appearance
   
+  /// Wrapper of `style` property setter.
   @discardableResult
   func style(_ style: UIActivityIndicatorView.Style) -> Builder {
     base.style = style
     return self
   }
   
+  /// Wrapper of `color` property setter.
   @discardableResult
   func color(_ color: UIColor!) -> Builder {
     base.color = color
