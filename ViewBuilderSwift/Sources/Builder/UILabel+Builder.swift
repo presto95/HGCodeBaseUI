76d2f0ad498b395c4a23dd6f697bb0dcea1c7079
Presto <yoohan95@gmail.com>
2019-08-14 12:03:32 +0900

    End of UILabel

--- ViewBuilderSwift/Sources/Builder/UILabel+Builder.swift
+++ ViewBuilderSwift/Sources/Builder/UILabel+Builder.swift
@@ -10,96 +10,52 @@
 
 public extension Builder where Base: UILabel {
   
-  // MARK: - Accessing the Text Attributes
+  // MARK: - Helper
   
+  /// Helper associated with **text and text style** setting feature.
+  ///
+  /// - `plain` case for `text` property setter.
+  /// - `attributed` case for `attributedText` property setter.
   @discardableResult
-  func text(_ text: Text) -> Builder {
+  func text(_ text: Text, style: TextStyle? = nil) -> Builder {
     switch text {
     case let .plain(text):
       base.text = text
     case let .attributed(text, attributes):
-      let attributedString = NSAttributedString(string: text, attributes: attributes)
-      base.attributedText = attributedString
+      base.attributedText = .init(string: text, attributes: attributes)
+    }
+    if let style = style {
+      self.textStyle(style)
     }
     return self
   }
   
+  /// Helper associated with **text style** setting feature.
+  ///
+  /// - `color` case for `textColor` property setter.
+  /// - `font` case for `font` property setter.
+  /// - `alignment` case for `textAlignment` property setter.
   @discardableResult
-  func font(_ font: UIFont!) -> Builder {
-    base.font = font
+  func textStyle(_ textStyle: TextStyle) -> Builder {
+    switch textStyle {
+    case let .each(color, font, alignment):
+      base.textColor = color
+      base.font = font
+      base.textAlignment = alignment
+    case let .color(color):
+      base.textColor = color
+    case let .font(font):
+      base.font = font
+    case let .alignment(alignment):
+      base.textAlignment = alignment
+    }
     return self
   }
   
-  @discardableResult
-  func textColor(_ color: UIColor!) -> Builder {
-    base.textColor = color
-    return self
-  }
-  
-  @discardableResult
-  func textAlignment(_ alignment: NSTextAlignment) -> Builder {
-    base.textAlignment = alignment
-    return self
-  }
-  
-  @discardableResult
-  func lineBreakMode(_ mode: NSLineBreakMode) -> Builder {
-    base.lineBreakMode = mode
-    return self
-  }
-  
-  @discardableResult
-  func isEnabled(_ flag: Bool) -> Builder {
-    base.isEnabled = flag
-    return self
-  }
-  
-  // MARK: - Sizing the Label's Text
-  
-  @discardableResult
-  func adjustsFontSizeToFitWidth(_ flag: Bool) -> Builder {
-    base.adjustsFontSizeToFitWidth = flag
-    return self
-  }
-  
-  @discardableResult
-  func allowsDefaultTighteningForTruncation(_ flag: Bool) -> Builder {
-    base.allowsDefaultTighteningForTruncation = flag
-    return self
-  }
-  
-  @discardableResult
-  func baselineAdjustment(_ adjustment: UIBaselineAdjustment) -> Builder {
-    base.baselineAdjustment = adjustment
-    return self
-  }
-  
-  @discardableResult
-  func minimumScaleFactor(_ factor: CGFloat) -> Builder {
-    base.minimumScaleFactor = factor
-    return self
-  }
-  
-  @discardableResult
-  func numberOfLines(_ lines: Int) -> Builder {
-    base.numberOfLines = lines
-    return self
-  }
-  
-  // MARK: - Managing Highlight Values
-  
-  @discardableResult
-  func highlightedTextColor(_ color: UIColor?) -> Builder {
-    base.highlightedTextColor = color
-    return self
-  }
-  
-  @discardableResult
-  func isHighlighted(_ flag: Bool) -> Builder {
-    base.isHighlighted = flag
-    return self
-  }
-  
+  /// Helper associated with **shadow** setting feature.
+  ///
+  /// - `color` case for `shadowColor` property setter.
+  /// - `offset` case for `shadowOffset` property setter.
   func shadow(_ shadow: LabelBuilder.Shadow<Int>) -> Builder {
     switch shadow {
     case let .each(color, offset):
@@ -127,6 +83,10 @@ public extension Builder where Base: UILabel {
     return self
   }
   
+  /// Helper associated with **shadow** setting feature.
+  ///
+  /// - `color` case for `shadowColor` property setter.
+  /// - `offset` case for `shadowOffset` property setter.
   func shadow(_ shadow: LabelBuilder.Shadow<Double>) -> Builder {
     switch shadow {
     case let .each(color, offset):
@@ -154,6 +114,10 @@ public extension Builder where Base: UILabel {
     return self
   }
   
+  /// Helper associated with **shadow** setting feature.
+  ///
+  /// - `color` case for `shadowColor` property setter.
+  /// - `offset` case for `shadowOffset` property setter.
   func shadow(_ shadow: LabelBuilder.Shadow<CGFloat>) -> Builder {
     switch shadow {
     case let .each(color, offset):
@@ -181,11 +145,139 @@ public extension Builder where Base: UILabel {
     return self
   }
   
+  // MARK: - Accessing the Text Attributes
+  
+  /// Wrapper of `text` property setter.
+  @discardableResult
+  func text(_ text: String?) -> Builder {
+    base.text = text
+    return self
+  }
+  
+  /// Wrapper of `attributedText` property setter.
+  @discardableResult
+  func attributedText(_ text: NSAttributedString?) -> Builder {
+    base.attributedText = text
+    return self
+  }
+  
+  /// Wrapper of `font` property setter.
+  @discardableResult
+  func font(_ font: UIFont!) -> Builder {
+    base.font = font
+    return self
+  }
+  
+  /// Wrapper of `textColor` property setter.
+  @discardableResult
+  func textColor(_ color: UIColor!) -> Builder {
+    base.textColor = color
+    return self
+  }
+  
+  /// Wrapper of `textAlignment` property setter.
+  @discardableResult
+  func textAlignment(_ alignment: NSTextAlignment) -> Builder {
+    base.textAlignment = alignment
+    return self
+  }
+  
+  /// Wrapper of `lineBreakMode` property setter.
+  @discardableResult
+  func lineBreakMode(_ mode: NSLineBreakMode) -> Builder {
+    base.lineBreakMode = mode
+    return self
+  }
+  
+  /// Wrapper of `isEnabled` property setter.
+  @discardableResult
+  func isEnabled(_ flag: Bool) -> Builder {
+    base.isEnabled = flag
+    return self
+  }
+  
+  // MARK: - Sizing the Label's Text
+  
+  /// Wrapper of `adjustsFontSizeToFitWidth` property setter.
+  @discardableResult
+  func adjustsFontSizeToFitWidth(_ flag: Bool) -> Builder {
+    base.adjustsFontSizeToFitWidth = flag
+    return self
+  }
+  
+  /// Wrapper of `allowsDefaultTighteningForTruncation` property setter.
+  @discardableResult
+  func allowsDefaultTighteningForTruncation(_ flag: Bool) -> Builder {
+    base.allowsDefaultTighteningForTruncation = flag
+    return self
+  }
+  
+  /// Wrapper of `baselineAdjustment` property setter.
+  @discardableResult
+  func baselineAdjustment(_ adjustment: UIBaselineAdjustment) -> Builder {
+    base.baselineAdjustment = adjustment
+    return self
+  }
+  
+  /// Wrapper of `minimumScaleFactor` property setter.
+  @discardableResult
+  func minimumScaleFactor(_ factor: CGFloat) -> Builder {
+    base.minimumScaleFactor = factor
+    return self
+  }
+  
+  /// Wrapper of `numberOfLines` property setter.
+  @discardableResult
+  func numberOfLines(_ lines: Int) -> Builder {
+    base.numberOfLines = lines
+    return self
+  }
+  
+  // MARK: - Managing Highlight Values
+  
+  /// Wrapper of `highlightedTextColor` property setter.
+  @discardableResult
+  func highlightedTextColor(_ color: UIColor?) -> Builder {
+    base.highlightedTextColor = color
+    return self
+  }
+  
+  /// Wrapper of `isHighlighted` property setter.
+  @discardableResult
+  func isHighlighted(_ flag: Bool) -> Builder {
+    base.isHighlighted = flag
+    return self
+  }
+  
+  // MARK: - Drawing a Shadow
+  
+  /// Wrapper of `shadowColor` property setter.
+  @discardableResult
+  func shadowColor(_ color: UIColor?) -> Builder {
+    base.shadowColor = color
+    return self
+  }
+  
+  /// Wrapper of `shadowOffset` property setter.
+  @discardableResult
+  func shadowOffset(_ offset: CGSize) -> Builder {
+    base.shadowOffset = offset
+    return self
+  }
+
   // MARK: - Getting the Layout Constraints
   
+  /// Wrapper of `preferredMaxLayoutWidth` property setter.
   @discardableResult
   func preferredMaxLayoutWidth(_ width: CGFloat) -> Builder {
     base.preferredMaxLayoutWidth = width
+    return self
+  }
+  
+  /// Wrapper of `isUserInteractionEnabled` property setter.
+  @discardableResult
+  func isUserInteractionEnabled(_ flag: Bool) -> Builder {
+    base.isUserInteractionEnabled = flag
     return self
   }
 }
