fa1aa6982817ab793d7c01a68e22b222e1a8feb0
Presto <yoohan95@gmail.com>
2019-08-14 20:46:24 +0900

    End of UISegmentedControl

--- ViewBuilderSwift/Sources/Builder/UISegmentedControl+Builder.swift
+++ ViewBuilderSwift/Sources/Builder/UISegmentedControl+Builder.swift
@@ -10,30 +10,94 @@
 
 public extension Builder where Base: UISegmentedControl {
   
-  // MARK: - Managing Segment Content & Managing Segments
+  // MARK: - Helper
   
+  /// Helper of `valueChanged` event of target-action pattern.
   @discardableResult
-  func segment(_ segmentSetting: SegmentedControlBuilder.Setting,
-               animated: Bool = false) -> Builder {
-    switch segmentSetting {
-    case let .insert(segment, index):
-      switch segment {
-      case let .image(image):
-        base.insertSegment(with: image, at: index, animated: animated)
-      case let .title(title):
-        base.insertSegment(withTitle: title, at: index, animated: animated)
-      }
-    case let .modify(segment, index):
-      switch segment {
-      case let .image(image):
-        base.setImage(image, forSegmentAt: index)
-      case let .title(title):
-        base.setTitle(title, forSegmentAt: index)
-      }
-    }
+  func valueChanged(_ target: Any?, selector: Selector) -> Builder {
+    base.addTarget(target, action: selector, for: .valueChanged)
     return self
   }
   
+  /// Helper of `setContentOffset(_:forSegmentAt:)` method.
+  @discardableResult
+  func contentOffset(_ offset: Size<Int>, forSegmentAt index: Int) -> Builder {
+    base.setContentOffset(offset.cgSize, forSegmentAt: index)
+    return self
+  }
+  
+  /// Helper of `setContentOffset(_:forSegmentAt:)` method.
+  @discardableResult
+  func contentOffset(_ offset: Size<Double>,
+                     forSegmentAt index: Int) -> Builder {
+    base.setContentOffset(offset.cgSize, forSegmentAt: index)
+    return self
+  }
+  
+  /// Helper of `setContentOffset(_:forSegmentAt:)` method.
+  @discardableResult
+  func contentOffset(_ offset: Size<CGFloat>,
+                     forSegmentAt index: Int) -> Builder {
+    base.setContentOffset(offset.cgSize, forSegmentAt: index)
+    return self
+  }
+  
+  /// Helper associated with **image** setting feature.
+  @discardableResult
+  func image(_ image: SegmentedControlBuilder.Image) -> Builder {
+    switch image {
+    case let .each(background, divider):
+      base.setBackgroundImage(background.0,
+                              for: background.state,
+                              barMetrics: background.barMetrics)
+      base.setDividerImage(divider.0,
+                           forLeftSegmentState: divider.leftSegmentState,
+                           rightSegmentState: divider.rightSegmentState,
+                           barMetrics: divider.barMetrcis)
+    case let .background(image, state, barMetrics):
+      base.setBackgroundImage(image, for: state, barMetrics: barMetrics)
+    case let .divider(image, leftSegmentState, rightSegmentState, barMetrcis):
+      base.setDividerImage(image,
+                           forLeftSegmentState: leftSegmentState,
+                           rightSegmentState: rightSegmentState,
+                           barMetrics: barMetrcis)
+    }
+    return self
+  }
+
+  // MARK: - Managing Segment Content
+  
+  /// Wrapper of `setImage(_:forSegmentAt:)` method.
+  @discardableResult
+  func image(_ image: UIImage?, forSegmentAt index: Int) -> Builder {
+    base.setImage(image, forSegmentAt: index)
+    return self
+  }
+  
+  /// Wrapper of `setTitle(_:forSegmentAt:)` method.
+  @discardableResult
+  func title(_ title: String?, forSegmentAt index: Int) -> Builder {
+    base.setTitle(title, forSegmentAt: index)
+    return self
+  }
+  
+  // MARK: - Managing Segments
+  
+  /// Wrapper of `insertSegment(with:at:animated:)` method.
+  @discardableResult
+  func segment(with image: UIImage?, at index: Int, animated: Bool) -> Builder {
+    base.insertSegment(with: image, at: index, animated: animated)
+    return self
+  }
+  
+  /// Wrapper of `insertSegment(withTitle:at:animated:)` method.
+  @discardableResult
+  func segment(withTitle title: String?, at index: Int, animated: Bool) -> Builder {
+    base.insertSegment(withTitle: title, at: index, animated: animated)
+    return self
+  }
+  
+  /// Wrapper of `selectedSegmentIndex` property setter.
   @discardableResult
   func selectedSegmentIndex(_ index: Int) -> Builder {
     base.selectedSegmentIndex = index
@@ -42,65 +106,35 @@ public extension Builder where Base: UISegmentedControl {
   
   // MARK: - Managing Segment Behavior and Appearance
   
+  /// Wrapper of `isMomentary` property setter.
   @discardableResult
   func isMomentary(_ flag: Bool) -> Builder {
     base.isMomentary = flag
     return self
   }
   
+  /// Wrapper of `setEnabled(_:forSegmentAt:)` method.
   @discardableResult
   func isEnabled(_ flag: Bool, forSegmentAt index: Int) -> Builder {
     base.setEnabled(flag, forSegmentAt: index)
     return self
   }
   
+  /// Wrapper of `setContentOffset(_:forSegmentAt:)` method.
   @discardableResult
-  func contentOffset(_ offset: Size<Int>, forSegmentAt index: Int) -> Builder {
-    switch offset {
-    case let .each(width, height):
-      base.setContentOffset(.init(width: width, height: height), forSegmentAt: index)
-    case let .all(value):
-      base.setContentOffset(.init(width: value, height: value), forSegmentAt: index)
-    case .zero:
-      base.setContentOffset(.zero, forSegmentAt: index)
-    }
+  func contentOffset(_ offset: CGSize, forSegmentAt index: Int) -> Builder {
+    base.setContentOffset(offset, forSegmentAt: index)
     return self
   }
   
-  @discardableResult
-  func contentOffset(_ offset: Size<Double>,
-                     forSegmentAt index: Int) -> Builder {
-    switch offset {
-    case let .each(width, height):
-      base.setContentOffset(.init(width: width, height: height), forSegmentAt: index)
-    case let .all(value):
-      base.setContentOffset(.init(width: value, height: value), forSegmentAt: index)
-    case .zero:
-      base.setContentOffset(.zero, forSegmentAt: index)
-    }
-    return self
-  }
-  
-  @discardableResult
-  func contentOffset(_ offset: Size<CGFloat>,
-                     forSegmentAt index: Int) -> Builder {
-    switch offset {
-    case let .each(width, height):
-      base.setContentOffset(.init(width: width, height: height), forSegmentAt: index)
-    case let .all(value):
-      base.setContentOffset(.init(width: value, height: value), forSegmentAt: index)
-    case .zero:
-      base.setContentOffset(.zero, forSegmentAt: index)
-    }
-    return self
-  }
-  
+  /// Wrapper of `setWidth(_:forSegmentAt:)` method.
   @discardableResult
   func width(_ width: CGFloat, forSegmentAt index: Int) -> Builder {
     base.setWidth(width, forSegmentAt: index)
     return self
   }
   
+  /// Wrapper of `apportionsSegmentWidthsByContent` property setter.
   @discardableResult
   func apportionsSegmentWidthsByContent(_ flag: Bool) -> Builder {
     base.apportionsSegmentWidthsByContent = flag
@@ -109,36 +143,47 @@ public extension Builder where Base: UISegmentedControl {
   
   // MARK: - Customizing Appearance
   
+  /// Wrapper of `tintColor` property setter.
+  @discardableResult
+  func tintColor(_ color: UIColor!) -> Builder {
+    base.tintColor = color
+    return self
+  }
+  
+  /// Wrapper of `setBackgroundImage(_:for:barMetrics:)` method.
   @discardableResult
   func backgroundImage(_ image: UIImage?,
-                       for state: UIControl.State = .normal,
-                       barMetrics: UIBarMetrics = .default) -> Builder {
+                       for state: UIControl.State,
+                       barMetrics: UIBarMetrics) -> Builder {
     base.setBackgroundImage(image, for: state, barMetrics: barMetrics)
     return self
   }
   
+  /// Wrapper of `setContentPositionAdjustment(_:forSegmentType:barMetrics:)` method.
   @discardableResult
   func contentPositionAdjustment(_ adjustment: UIOffset,
                                  forSegmentType segmentType: UISegmentedControl.Segment,
                                  barMetrics: UIBarMetrics) -> Builder {
     base.setContentPositionAdjustment(adjustment,
-                                                  forSegmentType: segmentType,
-                                                  barMetrics: barMetrics)
+                                      forSegmentType: segmentType,
+                                      barMetrics: barMetrics)
     return self
   }
   
+  /// Wrapper of `setDividerImage(_:forLeftSegmentState:rightSegmentState:)` method.
   @discardableResult
   func dividerImage(_ image: UIImage?,
                     forLeftSegmentState leftSegmentState: UIControl.State,
                     rightSegmentState: UIControl.State,
                     barMetrics: UIBarMetrics) -> Builder {
     base.setDividerImage(image,
-                                     forLeftSegmentState: leftSegmentState,
-                                     rightSegmentState: rightSegmentState,
-                                     barMetrics: barMetrics)
+                         forLeftSegmentState: leftSegmentState,
+                         rightSegmentState: rightSegmentState,
+                         barMetrics: barMetrics)
     return self
   }
   
+  /// Wrapper of `setTitleTextAttributes(_:for:)` method.
   @discardableResult
   func titleTextAttributes(_ attributes: [NSAttributedString.Key: Any],
                            for state: UIControl.State) -> Builder {
