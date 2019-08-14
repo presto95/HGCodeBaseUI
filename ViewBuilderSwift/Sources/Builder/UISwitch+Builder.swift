ad3a8a01a2d1170659be6ba0a35ade6a2f693ea7
Presto <yoohan95@gmail.com>
2019-08-14 20:46:54 +0900

    End of UISwitch

--- ViewBuilderSwift/Sources/Builder/UISwitch+Builder.swift
+++ ViewBuilderSwift/Sources/Builder/UISwitch+Builder.swift
@@ -9,31 +9,74 @@
 #if os(iOS)
 
 public extension Builder where Base: UISwitch {
+
+  // MARK: - Helper
   
-  // MARK: - Setting the Off/On State
-  
+  /// Helper of `valueChanged` event of target-action pattern.
   @discardableResult
-  func isOn(_ flag: Bool, animated: Bool = false) -> Builder {
-    base.setOn(flag, animated: animated)
+  func valueChanged(target: Any?, selector: Selector) -> Builder {
+    base.addTarget(target, action: selector, for: .valueChanged)
     return self
   }
   
-  // MARK: - Customizing the Appearance of the Switch
-  
+  /// Helper associated with **color** setting feature.
+  ///
+  /// - `on` case for `onTintColor` property setter.
+  /// - `off` case for `tintColor` property setter.
+  /// - `thumb` case for `thumbTintColor` property setter.
   @discardableResult
-  func tintColor(_ color: SwitchBuilder.Color) -> Builder {
+  func color(_ color: SwitchBuilder.Color) -> Builder {
     switch color {
     case let .each(on, off, thumb):
       base.onTintColor = on
       base.tintColor = off
       base.thumbTintColor = thumb
-    case let .on(color):
-      base.onTintColor = color
-    case let .off(color):
-      base.tintColor = color
-    case let .thumb(color):
-      base.thumbTintColor = color
+    case let .on(on):
+      base.onTintColor = on
+    case let .off(off):
+      base.tintColor = off
+    case let .thumb(thumb):
+      base.thumbTintColor = thumb
     }
+    return self
+  }
+
+  // MARK: - Setting the Off/On State
+  
+  /// Wrapper of `isOn` property setter.
+  @discardableResult
+  func isOn(_ flag: Bool) -> Builder {
+    base.isOn = flag
+    return self
+  }
+  
+  /// Wrapper of `setOn(_:animated:)` method.
+  @discardableResult
+  func isOn(_ flag: Bool, animated: Bool) -> Builder {
+    base.setOn(flag, animated: animated)
+    return self
+  }
+  
+  // MARK: - Customizing the Appearance of the Switch
+
+  /// Wrapper of `onTintColor` property setter.
+  @discardableResult
+  func onTintColor(_ color: UIColor?) -> Builder {
+    base.onTintColor = color
+    return self
+  }
+  
+  /// Wrapper of `tintColor` property setter.
+  @discardableResult
+  func tintColor(_ color: UIColor!) -> Builder {
+    base.tintColor = color
+    return self
+  }
+  
+  /// Wrapper of `thumbTintColor` property setter.
+  @discardableResult
+  func thumbTintColor(_ color: UIColor?) -> Builder {
+    base.thumbTintColor = color
     return self
   }
 }
